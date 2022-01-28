1490c9135ed71f8b29d5abb5d6e5d09dfec5b3be
eudes.neto <eudes.neto@ambevtech.com.br>
2022-01-28 13:55:20 -0300

    Finalizada o segundo curso de js orientado a objeto da Alura JavaScript: Interfaces e Herança em Orientação a Objetos

---
+++ Conta/Conta.js
@@ -0,0 +1,56 @@
+//Classe Abstrata
+
+export class Conta{
+  constructor(saldoInicial, cliente, agencia){
+    if(this.constructor == Conta){
+      throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstrata")
+    }
+
+    this._saldo = saldoInicial;
+    this._cliente = cliente;
+    this._agencia = agencia;
+  }
+
+  set cliente(novoValor){
+    if(novoValor instanceof Cliente){
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente(){
+    return this._cliente;
+  }
+
+  _saldo = 0;
+
+  get saldo(){
+    return this._saldo;
+  }
+
+  //Metódo Abstrado, não deve ser chamado, deve ser sobrescrito
+  sacar(valor){
+    throw new Error("O método Sacar da conta é abstrato")
+  }
+
+  _sacar(valor, taxa){
+    const valorSacado = taxa * valor;
+
+    if(this._saldo >= valorSacado){
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
+
+    return 0;
+  }
+
+  depositar(valor){
+    if(valor <= 100) return;
+
+    this._saldo += valor;
+  }
+
+  transferir(valor, conta){
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
+}
--- ContaPoupanca.js
+++
@@ -1,7 +0,0 @@
-export class ContaPoupanca{
-  constructor(saldoInicial, cliente, agencia){
-    this._saldo = saldoInicial;
-    this._cliente = cliente;
-    this._agencia = agencia;
-  }
-}
